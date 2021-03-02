9ae99080e383f92404bc41111c884e6b1f7c5486
y-ibuki <y.ibuki91@gmail.com>
2021-03-03 00:33:49 +0900

    プラグインインストール

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './styles/*.scss',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
