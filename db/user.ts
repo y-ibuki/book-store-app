590ab1951f3b53ab6c35d5186dbcd9d942b3ba43
y-ibuki <y.ibuki91@gmail.com>
2021-03-04 23:59:28 +0900

    user model作成

--- db/user.ts
+++ db/user.ts
@@ -1,11 +1,21 @@
 import { Firebase } from "../config/firebase";
+import 'firebase/firestore';
 
-export const GetUser = async (userId: number) => {
-    // const db = Firebase.firestore()
-    // const ref = await db.collection('users')
-        // .where('name', "==", {name: 'name'})
-        // .get()
-    // console.log(ref)
-    // return ref.docs[0]
-    return 'rrr'
+/**
+ * const [userName, setUserName] = useState('')
+ * const [userId, setUserId] = useState('')
+ * const user = GetUser('yugo');
+ * user.then( item => {
+ *      setUserName(item.name)
+ *      setUserId(item.id)
+ *  })
+ *
+ * @param userId
+ * @constructor
+ */
+export const GetUser = async (userId: string) => {
+    const db = await Firebase.firestore()
+    const collection = await db.collection('users').where('name', "==", userId).get()
+    const data = collection.docs[0].data()
+    return data;
 }
