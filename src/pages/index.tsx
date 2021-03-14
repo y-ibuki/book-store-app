f29a207508dd34f1e591126e83f7d1fdc0f4ae8c
y-ibuki <y.ibuki91@gmail.com>
2021-03-15 02:01:55 +0900

    一覧検索のベース作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import Layout from "../components/Layout";
 import LoginPage from '../components/Login'
 import BookIndex from '../components/BookIndex'
-import React, {SFC, useCallback} from "react";
+import React, {SFC, useCallback, useRef, useState, useEffect} from "react";
 import { auth } from "../../config/firebase";
 
 const IndexPage: SFC = () => {
-  const onClick = (e) => useCallback( () => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [price, setPrice] = useState('')
+  const [publishStart, setPublishStart] = useState('')
+  const [publishEnd, setPublishEnd] = useState('')
 
-  }, [])
+  const onClick = (e) => {
+      // firebaseに情報を取りに行く
+  }
+
+  const setTitleValue = (value) => { setTitle(value) }
+  const setAuthorValue = (value) => { setAuthor(value) }
+  const setPriceValue = (value) => { setPrice(value) }
+  const setPublishStartValue = (value) => { setPublishStart(value) }
+  const setPublishEndValue = (value) => { setPublishEnd(value) }
 
   return (
       <Layout>
@@ -21,25 +33,50 @@ const IndexPage: SFC = () => {
                               <div className="flex pt-3 items-end">
                                   <label className="w-1/5 mr-3">
                                       <p>書籍名</p>
-                                      <input type="text" className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"/>
+                                      <input
+                                          type="text"
+                                          name="title"
+                                          onChange={ (e) => { setTitleValue(e.target.value) }}
+                                          className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"
+                                      />
                                   </label>
                                   <label className="w-1/5 mr-3">
                                       <p>著者名</p>
-                                      <input type="text" className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"/>
+                                      <input
+                                          type="text"
+                                          name="author"
+                                          onChange={ (e) => { setAuthorValue(e.target.value) }}
+                                          className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"
+                                      />
                                   </label>
                                   <label className="w-1/5 mr-3">
                                       <p>値段</p>
-                                      <input type="text" className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"/>
+                                      <input
+                                          type="text"
+                                          name="price"
+                                          onChange={ (e) => { setPriceValue(e.target.value) }}
+                                          className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"
+                                      />
                                   </label>
                               </div>
                               <div className="flex pt-3 items-end">
                                   <label className="w-1/5 mr-3">
                                       <p>出版日 開始</p>
-                                      <input type="date" className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"/>
+                                      <input
+                                          type="date"
+                                          name="publish-start"
+                                          onChange={ (e) => { setPublishStartValue(e.target.value) }}
+                                          className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"
+                                      />
                                   </label>
                                   <label className="w-1/5 mr-3">
                                       <p>出版日 終了</p>
-                                      <input type="date" className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"/>
+                                      <input
+                                          type="date"
+                                          name="publish-end"
+                                          onChange={ (e) => { setPublishEndValue(e.target.value) }}
+                                          className="bg-gray-200 focus:bg-white border-2 p-1 rounded w-full"
+                                      />
                                   </label>
                                   <button onClick={onClick} className="bg-green-100 hover:bg-opacity-60 rounded px-4 py-2 h-10">検索</button>
                               </div>
