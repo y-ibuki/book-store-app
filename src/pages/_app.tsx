e9ad1d1c26a79a06f1e1008c82b271e51401e24a
y-ibuki <y.ibuki91@gmail.com>
2021-03-20 23:31:49 +0900

    authContextとbookContextに分ける

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,33 @@ import React, { useReducer, useEffect } from "react";
 
 import { AppProps } from "next/app";
 
-import AuthContext from "~/src/lib/AuthContext";
-import authReducer from "~/src/lib/authReducer";
+import AuthContext from "~/src/lib/Auth/AuthContext";
+import authReducer from "~/src/lib/Auth/authReducer";
 import { listenAuthState } from "../../config/firebase";
+
+import BookContext from "~/src/lib/Book/BookContext";
+import bookReducer from "~/src/lib/Book/bookReducer";
 
 import "../../assets/styles/globals.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [state, dispatch] = useReducer(
+  const [authState, authDispatch] = useReducer(
     authReducer.reducer,
     authReducer.initialState
   );
+  const [bookState, dispatch] = useReducer(
+      bookReducer.reducer,
+      bookReducer.initialState
+  );
   useEffect(() => {
-    return listenAuthState(dispatch);
+    return listenAuthState(authDispatch);
   }, []);
   return (
-    <AuthContext.Provider value={state}>
-      <Component {...pageProps} />
-    </AuthContext.Provider>
+      <BookContext.Provider value={bookState}>
+        <AuthContext.Provider value={authState}>
+          <Component {...pageProps} />
+        </AuthContext.Provider>
+      </BookContext.Provider>
   );
 }
 
